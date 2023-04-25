c3ace8691b9090bc2a1153c6a8eeabf674e17c56
Gustavo Henrique <gustavo@gustavohenrique.net>
2023-04-25 16:52:00 -0300

    Replaces input from function args to stdin reading

--- src/autorizador/core.clj
+++ src/autorizador/core.clj
@@ -1,16 +1,8 @@
 (ns autorizador.core
-  (:use autorizador.controller)
-  (:require [clojure.string :refer [join split]]))
+  (:use autorizador.controller))
 
-
-(defn -main
-  "Autoriza ou rejeita uma transacao"
-  ([] (println "é obrigatório passar uma string JSON como argumento."))
-  ([& args]
-    (let [param (join "" args)
-          lines (split param #"}}")
-          json (mapv #(str % "}}") lines)
-          output (do-action json)]
-      (doseq [out output]
-        (println out)))
-  ))
+(defn -main []
+  (let [lines (for [line (line-seq (java.io.BufferedReader. *in*))] line)
+        output (do-action lines)]
+    (doseq [out output]
+      (println out))))
