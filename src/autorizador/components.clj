1727a3fea5b7343f174a9eb0c353e94eb26f51cf
Gustavo Henrique <gustavo@gustavohenrique.net>
2023-05-02 09:22:55 -0300

    Move json management from controller to components

---
+++ src/autorizador/components.clj
@@ -0,0 +1,11 @@
+(ns autorizador.components
+  (:require [cheshire.core :refer [parse-string generate-string]]))
+
+(defn str->json [input]
+  (try
+    (parse-string input true)
+    (catch Exception e [e])
+  ))
+
+(defn json->str [input]
+  (generate-string input))
--- src/autorizador/controller.clj
+++ src/autorizador/controller.clj
@@ -1,18 +1,9 @@
 (ns autorizador.controller
   (:use autorizador.logic)
-  (:require [cheshire.core :refer [parse-string generate-string]]))
-
-(defn- parse [input]
-  (try
-    (parse-string input true)
-    (catch Exception e [e])
-  ))
-
-(defn- print-json [input]
-  (println (generate-string input)))
+  (:use autorizador.components))
 
 (defn do-action [json]
-  (let [operations (map #(parse %) json)
+  (let [operations (map #(str->json %) json)
         states (process-operations [] operations)
         accounts (map #(dissoc % :transaction) states)]
-    (mapv generate-string accounts)))
+    (mapv json->str accounts)))
